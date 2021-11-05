803feee588fd0ce3548f28b7ebfc8db2d32be3f6
Tanijl Tuly <tanjiltuly@gmail.com>
2021-11-05 20:50:41 +0600

    __--__ server fix

--- index.js
+++ index.js
@@ -9,5 +9,20 @@ app.get('/', (req, res) => {
     res.json(articles)
 })
 
+app.get('/api/articles/:id', (req, res) => {
+
+    const id = req.params.id;
+    const filtered = articles.filter((article) => article.id === id)
+
+  if (filtered.length > 0) {
+    res.status(200).json(filtered[0])
+  } else {
+    res
+      .status(404)
+      .json({ message: `Article with the id of ${id} is not found` })
+  }
+})
+
+
 
 app.listen(port, console.log('server running on port', port))
